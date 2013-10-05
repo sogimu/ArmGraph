fc9c600220d51a685ac4742c2b7d99cac89e194d
Alexander Lizin <sogimu@nxt.ru>
2013-10-05 22:18:47 +0700

    Connected Closure compiler as submodule

--- compile.bat
+++
@@ -1,8 +0,0 @@
-::--compilation_level
-::WHITESPACE_ONLY
-::SIMPLE_OPTIMIZATIONS
-::ADVANCED_OPTIMIZATIONS
-
-java -jar compiler.jar --js frameworks/Gizmo/gizmo-0.2.8.js --js modules/ArmLib/ArmLib.js --js modules/ArmObj/ArmObj.js --js modules/ArmObj/VisualObj/Skeleton.js --js modules/ArmObj/VisualObj/VisualObj.js --js modules/ArmObj/VisualObj/Primitive/Primitive.js --js modules/ArmObj/VisualObj/Object/Object.js --js modules/Layer/EventStack.js --js modules/Layer/Layer.js --js modules/ArmObj/VisualObj/Primitive/Rect/Rect.js --js modules/ArmObj/VisualObj/Primitive/Image/Image.js --js modules/ArmObj/VisualObj/Primitive/Line/Line.js --js modules/ArmObj/VisualObj/Primitive/Circle/Circle.js --compilation_level SIMPLE_OPTIMIZATIONS  --language_in ECMASCRIPT5 --js_output_file ../armlib-0.1.0.js
-
-pause
--- compile.sh
+++ compile.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# --compilation_level
-# WHITESPACE_ONLY
-# SIMPLE_OPTIMIZATIONS
-# ADVANCED_OPTIMIZATIONS
-
-NAME="armcontext"
-VERSION="0.5.0"
+NAME="armgraph"
+VERSION="0.1.0"
 RASHIR="js"
 
 FILE_NAME=$NAME-$VERSION.$RASHIR
-FILE_NAME_WITH_OUT_VERSION=$NAME.$RASHIR
+FILE_NAME_MIN=$NAME".min"-$VERSION.$RASHIR
+FILE_NAME_WITHOUT_VERSION=$NAME.$RASHIR
 
-#COMPILATION_LEVEL="WHITESPACE_ONLY"
-COMPILATION_LEVEL="SIMPLE_OPTIMIZATIONS"
-#COMPILATION_LEVEL="ADVANCED_OPTIMIZATIONS"
+PATH_TO_COMPILER="./lib/Closure-compiler/"
+
+COMPILATION_LEVEL="WHITESPACE_ONLY"
+# COMPILATION_LEVEL="SIMPLE_OPTIMIZATIONS"
+# COMPILATION_LEVEL="ADVANCED_OPTIMIZATIONS"
+
+echo "Building..."
+java -jar "$PATH_TO_COMPILER"compiler.jar --js modules/ArmGraph/ArmGraph.js --js modules/Root/EventStack.js --js modules/Root/Root.js --js modules/ArmObject/ArmObject.js --compilation_level "$COMPILATION_LEVEL" --language_in ECMASCRIPT5 --js_output_file ./$FILE_NAME_MIN
+
+echo "$FILE_NAME_MIN"
+
+echo "$FILE_NAME_WITHOUT_VERSION"
+cp "$FILE_NAME_MIN" "$FILE_NAME_WITHOUT_VERSION"
 
 echo "$FILE_NAME"
-java -jar compiler.jar --js modules/ArmContext/ArmContext.js --js modules/Primitve/InternalRepresentation.js --js modules/Primitve/GlobalRepresentation.js --js modules/Primitve/C2dContextRepresentation.js --js modules/Primitve/MvMatrix.js --js modules/Primitve/Debug.js --js modules/Primitve/Primitive.js --js modules/Primitve/Image/Image2dContextRepresentation.js --js modules/Primitve/Image/ImageGlobalRepresentation.js --js modules/Primitve/Image/ImageInternalRepresentation.js --js modules/Primitve/Image/Image.js --js modules/Primitve/Rect/Rect2dContextRepresentation.js --js modules/Primitve/Rect/RectGlobalRepresentation.js --js modules/Primitve/Rect/RectInternalRepresentation.js --js modules/Primitve/Rect/Rect.js --js modules/Layer/Loop.js --js modules/Layer/Layer.js --compilation_level "$COMPILATION_LEVEL" --language_in ECMASCRIPT5 --js_output_file ./$FILE_NAME
-
-echo "$FILE_NAME_WITH_OUT_VERSION"
-cp "$FILE_NAME" "$FILE_NAME_WITH_OUT_VERSION"
+cp "$FILE_NAME_MIN" "$FILE_NAME"
